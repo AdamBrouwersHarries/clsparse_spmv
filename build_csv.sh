fcb4ca79d53154395fa7f89c166e86d72d44a476
Adam Harries <harries.adam@gmail.com>
2017-10-29 16:11:16 +0000

    add script to parse/build a csv file of results

---
+++ build_csv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Build a query from a folder containing results
+# get the directory containing (recursively) the results
+resdir=$1
+
+# make a temporary folder for the results
+bdir=$(basename $resdir)
+tdir="/tmp/clsparse_spmv/$bdir"
+echo "Making temp directory: $tdir"
+mkdir -p $tdir
+csvf=$tdir/results.csv
+
+# grep the results from the folder recursively, 
+# and subsitute various parts of it
+
+grep -r "TIMING_RESULT" $resdir | sed "s/.*TIMING_RESULT:/TIMING_RESULT:/g" > $csvf
+
+cp $csvf .
