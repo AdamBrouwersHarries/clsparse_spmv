7701d6383b951a17ea5aadbd47da9cc7e7e190fd
Adam Harries <harries.adam@gmail.com>
2017-10-29 16:39:35 +0000

    make build_csv now actually build a query

--- build_query.sh
+++ build_query.sh
@@ -3,17 +3,19 @@
 # Build a query from a folder containing results
 # get the directory containing (recursively) the results
 resdir=$1
+# get the name of a table to insert into
+table=$2
 
 # make a temporary folder for the results
 bdir=$(basename $resdir)
 tdir="/tmp/clsparse_spmv/$bdir"
 echo "Making temp directory: $tdir"
 mkdir -p $tdir
-csvf=$tdir/results.csv
+qfile=$tdir/query.sql
 
 # grep the results from the folder recursively, 
 # and subsitute various parts of it
 
-grep -ra "TIMING_RESULT" $resdir | sed "s/.*TIMING_RESULT\: //g" > $csvf
+grep -r "INSERT" $resdir | sed "s/.*INSERT/INSERT/g" | sed "s/table_name/$table/g" > > $qfile
 
-cp $csvf .
+cp $qfile .
